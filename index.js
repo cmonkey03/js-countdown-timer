abfc9e764c9bdd3831f262e2ddc15c6ab28fe12a
github username <github email address>
2019-02-16 16:34:09 -0500

    Second exmaple imported

--- index.js
+++ index.js
@@ -1,42 +1,102 @@
-function countdown(endDate) {
-  let days, hours, minutes, seconds;
+// function countdown(endDate) {
+//   let days, hours, minutes, seconds;
+//
+//   endDate = new Date(endDate).getTime();
+//
+//   if (isNaN(endDate)) {
+// 	return;
+//   }
+//
+//   setInterval(calculate, 1000);
+//
+//   function calculate() {
+//     let startDate = new Date();
+//     startDate = startDate.getTime();
+//
+//     let timeRemaining = parseInt((endDate - startDate) / 1000);
+//
+//     if (timeRemaining >= 0) {
+//       days = parseInt(timeRemaining / 86400);
+//       timeRemaining = (timeRemaining % 86400);
+//
+//       hours = parseInt(timeRemaining / 3600);
+//       timeRemaining = (timeRemaining % 3600);
+//
+//       minutes = parseInt(timeRemaining / 60);
+//       timeRemaining = (timeRemaining % 60);
+//
+//       seconds = parseInt(timeRemaining);
+//
+//       document.getElementById("days").innerHTML = parseInt(days, 10);
+//       document.getElementById("hours").innerHTML = ("0" + hours).slice(-2);
+//       document.getElementById("minutes").innerHTML = ("0" + minutes).slice(-2);
+//       document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2);
+//     } else {
+//       return;
+//     }
+//   }
+// }
+//
+// (function () {
+//   countdown('04/01/2019 05:00:00 PM');
+// }());
 
-  endDate = new Date(endDate).getTime();
 
-  if (isNaN(endDate)) {
-	return;
+// Second example project
+
+const countDownClock = (number = 100, format = 'seconds') => {
+
+  const d = document;
+  const daysElement = d.querySelector('.days');
+  const hoursElement = d.querySelector('.hours');
+  const minutesElement = d.querySelector('.minutes');
+  const secondsElement = d.querySelector('.seconds');
+  let countdown;
+  convertFormat(format);
+
+
+  function convertFormat(format) {
+    switch(format) {
+      case 'seconds':
+        return timer(number);
+      case 'minutes':
+        return timer(number * 60);
+        case 'hours':
+        return timer(number * 60 * 60);
+      case 'days':
+        return timer(number * 60 * 60 * 24);
+    }
   }
 
-  setInterval(calculate, 1000);
+  function timer(seconds) {
+    const now = Date.now();
+    const then = now + seconds * 1000;
 
-  function calculate() {
-    let startDate = new Date();
-    startDate = startDate.getTime();
+    countdown = setInterval(() => {
+      const secondsLeft = Math.round((then - Date.now()) / 1000);
 
-    let timeRemaining = parseInt((endDate - startDate) / 1000);
+      if(secondsLeft <= 0) {
+        clearInterval(countdown);
+        return;
+      };
 
-    if (timeRemaining >= 0) {
-      days = parseInt(timeRemaining / 86400);
-      timeRemaining = (timeRemaining % 86400);
+      displayTimeLeft(secondsLeft);
 
-      hours = parseInt(timeRemaining / 3600);
-      timeRemaining = (timeRemaining % 3600);
+    },1000);
+  }
 
-      minutes = parseInt(timeRemaining / 60);
-      timeRemaining = (timeRemaining % 60);
-
-      seconds = parseInt(timeRemaining);
-
-      document.getElementById("days").innerHTML = parseInt(days, 10);
-      document.getElementById("hours").innerHTML = ("0" + hours).slice(-2);
-      document.getElementById("minutes").innerHTML = ("0" + minutes).slice(-2);
-      document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2);
-    } else {
-      return;
-    }
+  function displayTimeLeft(seconds) {
+    daysElement.textContent = Math.floor(seconds / 86400);
+    hoursElement.textContent = Math.floor((seconds % 86400) / 3600);
+    minutesElement.textContent = Math.floor((seconds % 86400) % 3600 / 60);
+    secondsElement.textContent = seconds % 60 < 10 ? `0${seconds % 60}` : seconds % 60;
   }
 }
 
-(function () {
-  countdown('04/01/2333 05:00:00 PM');
-}());
+
+/*
+  start countdown
+  enter number and format
+  days, hours, minutes or seconds
+*/
+countDownClock(20, 'days');
