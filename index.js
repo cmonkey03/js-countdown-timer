0a86c208a5029b8f3413af69b09be5c538e618bb
github username <github email address>
2019-02-16 18:27:40 -0500

    Added renderCountdown function

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// For timezone support see:
+// https://stackoverflow.com/questions/18292716/javascript-countdown-with-specific-timezone?lq=1
+
+function renderCountdown(endDate) {
+  countdown('04/01/2019 05:00:00 PM PST');
+}
+
 function countdown(endDate) {
   let days, hours, minutes, seconds;
 
@@ -38,5 +45,5 @@ function countdown(endDate) {
 }
 
 (function () {
-  countdown('04/01/2019 05:00:00 PM');
+  renderCountdown('04/01/2019 05:00:00 PM PST');
 }());
