e7e98e7c65976af7d1e81a1351d7b1752d173277
github username <github email address>
2019-02-20 20:35:09 -0500

    Added input message text

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const form = document.getElementById('form')
   const inputMessage= document.getElementById("input-message")
 
+  inputMessage.innerHTML = "Please select a date and time in the future"
   form.querySelector("#datetime").value = today.toISOString().substring(0, 16)
 
   form.addEventListener('input', function(event) {
@@ -47,6 +48,7 @@ document.addEventListener('DOMContentLoaded', function() {
       let timeRemaining = parseInt((endDate - startDate) / 1000);
 
       if (timeRemaining >= 0) {
+
         days = parseInt(timeRemaining / 86400);
         timeRemaining = (timeRemaining % 86400);
 
