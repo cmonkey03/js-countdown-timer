7cc485ffe8e92cb805e4cbbd2150c2aa01777cbb
Russell Brown <info@russellpatrickbrown.com>
2019-04-21 20:39:02 -0400

    Removed extra queries

--- gf-timed-content/js/test.js
+++ gf-timed-content/js/test.js
@@ -1,13 +1,6 @@
 function CountdownTimer(endDate) {
-  let days, hours, minutes, seconds;
-
-  let dayTimer = document.getElementById("days")
-  let hourTimer = document.getElementById("hours")
-  let minuteTimer = document.getElementById("minutes")
-  let secondTimer = document.getElementById("seconds")
-  let titleBar= document.getElementById("title-bar")
-
   endDate = new Date(endDate).getTime();
+	let timer = document.getElementById(the_div)
 
   if (isNaN(endDate)) {
   return;
@@ -53,8 +46,6 @@ function CountdownTimer(endDate) {
   }
 }
 
-console.log(CountdownTimer)
-
 function gf_timed_content_init (json_arg) {
 	const args = JSON.parse(json_arg);
 	// alert ('inside init with a target date: ' + args['target_date']);
@@ -68,13 +59,5 @@ function gf_timed_content_countdown(json_arg) {
 	const date = args.to_date.substr(4, 21)
 	const dateObj = new Date(date)
 
-	console.log(date)
-	console.log(dateObj)
-	console.log(the_div)
-
-	debugger;
-
-	setTimeout(function() {
-		document.getElementById(the_div).innerHTML='imagine this is a countdown timer!';
-	}, 3000)
+	setTimeout(CountdownTimer, 3000)
 }
